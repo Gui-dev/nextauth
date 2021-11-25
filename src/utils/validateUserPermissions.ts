0f3b57fa004c594f476951f2296bf56714289542
Gui-dev <adrianon10@gmail.com>
2021-11-25 03:25:52 -0300

    feat: add helper function validateUserPermissions

---
+++ src/utils/validateUserPermissions.ts
@@ -0,0 +1,36 @@
+type User = {
+  permissions: string[]
+  roles: string[]
+}
+
+type validateUserPermissionsProps = {
+  user: User | undefined
+  permissions?: string[]
+  roles?: string[]
+}
+
+export const validateUserPermissions = (
+  { user, permissions= [], roles= [] }: validateUserPermissionsProps) => {
+
+  if (permissions.length > 0) {
+    const hasAllPermissions = permissions?.every(permission => {
+      return user?.permissions.includes(permission)
+    })
+
+    if (!hasAllPermissions) {
+      return false
+    }
+  }
+
+  if (roles.length > 0) {
+    const hasAllRoles = roles?.some(role => {
+      return user?.roles.includes(role)
+    })
+
+    if (!hasAllRoles) {
+      return false
+    }
+  }
+
+  return true
+}
